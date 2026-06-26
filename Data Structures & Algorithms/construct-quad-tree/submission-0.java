5c0f9bf72feb6aac993ca0c8ab0d414b7b0b41de
komal <kbhati9494@gmail.com>
2026-06-26 22:39:36 +0530

    Add: construct-quad-tree - submission-0

---
+++ Data Structures & Algorithms/construct-quad-tree/submission-0.java
@@ -0,0 +1,84 @@
+/*
+// Definition for a QuadTree node.
+class Node {
+    public boolean val;
+    public boolean isLeaf;
+    public Node topLeft;
+    public Node topRight;
+    public Node bottomLeft;
+    public Node bottomRight;
+
+    
+    public Node() {
+        this.val = false;
+        this.isLeaf = false;
+        this.topLeft = null;
+        this.topRight = null;
+        this.bottomLeft = null;
+        this.bottomRight = null;
+    }
+    
+    public Node(boolean val, boolean isLeaf) {
+        this.val = val;
+        this.isLeaf = isLeaf;
+        this.topLeft = null;
+        this.topRight = null;
+        this.bottomLeft = null;
+        this.bottomRight = null;
+    }
+    
+    public Node(boolean val, boolean isLeaf, Node topLeft, Node topRight, Node bottomLeft, Node bottomRight) {
+        this.val = val;
+        this.isLeaf = isLeaf;
+        this.topLeft = topLeft;
+        this.topRight = topRight;
+        this.bottomLeft = bottomLeft;
+        this.bottomRight = bottomRight;
+    }
+}
+*/
+
+class Solution {
+
+    public Node construct(int[][] grid) {
+        return build(grid, 0, 0, grid.length);
+    }
+
+    private Node build(int[][] grid, int row, int col, int size) {
+
+        // Check if the current square has the same value
+        boolean same = true;
+        int value = grid[row][col];
+
+        for (int i = row; i < row + size && same; i++) {
+            for (int j = col; j < col + size; j++) {
+                if (grid[i][j] != value) {
+                    same = false;
+                    break;
+                }
+            }
+        }
+
+        // Leaf node
+        if (same) {
+            return new Node(value == 1, true);
+        }
+
+        // Divide into 4 quadrants
+        int half = size / 2;
+
+        Node topLeft = build(grid, row, col, half);
+        Node topRight = build(grid, row, col + half, half);
+        Node bottomLeft = build(grid, row + half, col, half);
+        Node bottomRight = build(grid, row + half, col + half, half);
+
+        return new Node(
+                true,           // Value is arbitrary for non-leaf nodes
+                false,
+                topLeft,
+                topRight,
+                bottomLeft,
+                bottomRight
+        );
+    }
+}
